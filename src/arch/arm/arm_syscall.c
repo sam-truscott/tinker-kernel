a3665411263e38acf1c0ffdd5e579d6f107e8b59
Sam Truscott <sam@wumpus.co.uk>
2015-04-20 20:56:58 +0100

    Remove the thumb code

--- src/arch/arm/arm_syscall.c
+++ src/arch/arm/arm_syscall.c
@@ -22,7 +22,6 @@ uint32_t TINKER_API_CALL_7(
 		uint32_t param_7)
 {
 	kernel_assert("tinker: syscall error\n", api<MAX_SYSCALL);
-#if !defined(__thumb__)
 	register long _r0 __asm__("r0")=(long)(api);
 	register long _r7 __asm__("r7")=(long)(param_7);
 	register long _r6 __asm__("r6")=(long)(param_6);
@@ -38,21 +37,5 @@ uint32_t TINKER_API_CALL_7(
 			"r"(_r2), "r"(_r3), "r"(_r4), "r"(_r5),
 			"r"(_r6), "r"(_r7)
 			: "memory");
-#else
-	register long _r7 __asm__("r7")=(long)(api);
-	register long _r6 __asm__("r6")=(long)(param_7);
-	register long _r5 __asm__("r5")=(long)(param_6);
-	register long _r4 __asm__("r4")=(long)(param_5);
-	register long _r3 __asm__("r3")=(long)(param_4);
-	register long _r2 __asm__("r2")=(long)(param_3);
-	register long _r1 __asm__("r1")=(long)(param_2);
-	register long _r0 __asm__("r0")=(long)(param_1);
-	__asm__ __volatile__(
-			"svc 0"
-			: "=r"(_r0)
-			: "r"(_r0), "r"(_r1), "r"(_r2), "r"(_r3),
-			"r"(_r4), "r"(_r5), "r"(_r6), "r"(_r7)
-			: "memory");
-#endif
 	return (long) _r0;
 }
