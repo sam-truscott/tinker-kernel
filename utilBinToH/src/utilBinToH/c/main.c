3e6f3357cdc93eea8cbe6196ca4a5efa9d6fa458
Sam Truscott <sam@wumpus.co.uk>
2016-01-27 21:22:25 +0000

    Tidy up the code

--- utilBinToH/src/utilBinToH/c/main.c
+++ utilBinToH/src/utilBinToH/c/main.c
@@ -3,17 +3,35 @@
 
 int main(int argc, char** argv) {
     assert(argc == 2);
-    char* fn = argv[1];
-    FILE* f = fopen(fn, "rb");
+    if (argc != 2)
+    {
+    	printf("UtilBinToH <bin>\n");
+    	return -1;
+    }
+    char * const fn = argv[1];
+    FILE * const f = fopen(fn, "rb");
+    if (!f)
+    {
+    	printf("Failed to open %s\n", fn);
+    	return -2;
+    }
     printf("char a[] = {\n");
     unsigned long n = 0;
-    while(!feof(f)) {
+    while(!feof(f))
+    {
         unsigned char c;
-        if(fread(&c, 1, 1, f) == 0) break;
+        if (fread(&c, 1, 1, f) == 0)
+        {
+        	break;
+        }
         printf("0x%.2X,", (int)c);
         ++n;
-        if(n % 10 == 0) printf("\n");
+        if((n % 10) == 0)
+        {
+        	printf("\n");
+        }
     }
     fclose(f);
     printf("};\n");
+    return 0;
 }
