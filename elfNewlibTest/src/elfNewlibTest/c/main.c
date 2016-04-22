e20aae11d8838c92d552c8c596f27a52db8e670a
Sam Truscott <sam@wumpus.co.uk>
2016-04-22 22:45:22 +0100

    Add new example

---
+++ elfNewlibTest/src/elfNewlibTest/c/main.c
@@ -0,0 +1,16 @@
+/*
+ *
+ * TINKER Source Code
+ *
+ *
+ *  [2009] - [2016] Samuel Steven Truscott
+ *  All Rights Reserved.
+ */
+
+#include "stdio.h"
+
+void main(void)
+{
+	printf("hello world\n");
+	return;
+}
