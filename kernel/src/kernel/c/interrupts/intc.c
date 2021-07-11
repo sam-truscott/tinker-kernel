4d40829c6d954cab65f3bae0a2877de8bd8283eb
Sam Truscott <sam@wumpus.co.uk>
2021-07-11 18:59:31 +0100

    Extra intc range checks

--- kernel/src/kernel/c/interrupts/intc.c
+++ kernel/src/kernel/c/interrupts/intc.c
@@ -65,6 +65,10 @@ void intc_delete(intc_t * const intc)
 
 void intc_add_child(intc_t * const intc, const uint32_t cause, const intc_t * const child)
 {
+	if (cause > MAX_ISRS)
+	{
+		kernel_assert("add interrupt out of range", false);
+	}
 	if (intc)
 	{
 		intc_internal_t * const internal = mem_alloc(intc->pool, sizeof(intc_internal_t));
@@ -84,6 +88,10 @@ void intc_add_child(intc_t * const intc, const uint32_t cause, const intc_t * co
 
 void intc_add_device(intc_t * const intc, const uint32_t cause, const kernel_device_t * const device)
 {
+	if (cause > MAX_ISRS)
+	{
+		kernel_assert("add interrupt out of range", false);
+	}
 	if (intc)
 	{
 		intc_internal_t * const internal = mem_alloc(intc->pool, sizeof(intc_internal_t));
@@ -103,6 +111,10 @@ void intc_add_device(intc_t * const intc, const uint32_t cause, const kernel_dev
 
 void intc_add_pipe(intc_t * const intc, const uint32_t cause, const object_pipe_t * const pipe)
 {
+	if (cause > MAX_ISRS)
+	{
+		kernel_assert("add interrupt out of range", false);
+	}
 	if (intc)
 	{
 		intc_internal_t * const internal = mem_alloc(intc->pool, sizeof(intc_internal_t));
@@ -122,6 +134,10 @@ void intc_add_pipe(intc_t * const intc, const uint32_t cause, const object_pipe_
 
 void intc_add_timer(intc_t * const intc, const uint32_t cause, const timer_t * const timer)
 {
+	if (cause > MAX_ISRS)
+	{
+		kernel_assert("add interrupt out of range", false);
+	}
 	if (intc)
 	{
 		intc_internal_t * const internal = mem_alloc(intc->pool, sizeof(intc_internal_t));
@@ -147,43 +163,50 @@ return_t intc_handle(const intc_t * const intc, tgt_context_t * const context)
 		uint32_t cause = 0;
 		if (intc->kernel_intc->get_cause(&cause, intc->kernel_intc->user_data))
 		{
-			intc_internal_t * const dev = intc->isrs[cause];
-			if (dev != NULL)
+			if (cause > MAX_ISRS)
 			{
-				switch(dev->type)
-				{
-				case DEVICE_INTC:
-					ret = dev->devices.device->isr(dev->devices.device->user_data, cause);
-					break;
-				case CHILD_INTC:
-					ret = intc_handle(dev->devices.child, context);
-					break;
-				case PIPE_INTC:
-					ret = obj_pipe_send_message(
-							(object_pipe_t*)dev->devices.pipe,
-							0, /* no need, block is false */
-							PIPE_TX_SEND_AVAILABLE,
-							(void*)&cause, sizeof(cause),
-							false);
-					break;
-				case TIMER_INTC:
-					if (dev->devices.timer && dev->devices.timer->timer_isr)
-					{
-						ret = dev->devices.timer->timer_isr(context, dev->devices.timer->usr_data);
-					}
-					else
-					{
-						ret = UNKNOWN_INTERRUPT_CAUSE;
-					}
-					break;
-				case EMPTY_DEVICE:
-					ret = UNKNOWN_INTERRUPT_CAUSE;
-					break;
-				}
+				ret = UNKNOWN_INTERRUPT_CAUSE;
 			}
 			else
 			{
-				ret = UNKNOWN_INTERRUPT_CAUSE;
+				intc_internal_t * const dev = intc->isrs[cause];
+				if (dev != NULL)
+				{
+					switch(dev->type)
+					{
+					case DEVICE_INTC:
+						ret = dev->devices.device->isr(dev->devices.device->user_data, cause);
+						break;
+					case CHILD_INTC:
+						ret = intc_handle(dev->devices.child, context);
+						break;
+					case PIPE_INTC:
+						ret = obj_pipe_send_message(
+								(object_pipe_t*)dev->devices.pipe,
+								0, /* no need, block is false */
+								PIPE_TX_SEND_AVAILABLE,
+								(void*)&cause, sizeof(cause),
+								false);
+						break;
+					case TIMER_INTC:
+						if (dev->devices.timer && dev->devices.timer->timer_isr)
+						{
+							ret = dev->devices.timer->timer_isr(context, dev->devices.timer->usr_data);
+						}
+						else
+						{
+							ret = UNKNOWN_INTERRUPT_CAUSE;
+						}
+						break;
+					case EMPTY_DEVICE:
+						ret = UNKNOWN_INTERRUPT_CAUSE;
+						break;
+					}
+				}
+				else
+				{
+					ret = UNKNOWN_INTERRUPT_CAUSE;
+				}
 			}
 			intc->kernel_intc->ack_cause(cause, intc->kernel_intc->user_data);
 		}
