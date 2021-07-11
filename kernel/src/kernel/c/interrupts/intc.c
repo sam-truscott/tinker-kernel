b2ab449bd6c65d7705e9672a8273e6b2e5231ff0
Sam Truscott <sam@wumpus.co.uk>
2021-07-11 18:51:25 +0100

    Remove the map from the interrupt handler - there's only 256!

--- kernel/src/kernel/c/interrupts/intc.c
+++ kernel/src/kernel/c/interrupts/intc.c
@@ -9,7 +9,6 @@
 #include "interrupts/intc.h"
 
 #include "kernel_assert.h"
-#include "utils/collections/hashed_map.h"
 
 typedef struct
 {
@@ -23,26 +22,10 @@ typedef struct
 	} devices;
 } intc_internal_t;
 
-HASH_MAP_TYPE_T(isr_map_t)
-HASH_MAP_INTERNAL_TYPE_T(isr_map_t, uint32_t, intc_internal_t*, MAX_ISRS, 16)
-HASH_MAP_SPEC_CREATE(static, isr_map_t)
-HASH_MAP_SPEC_INITALISE(static, isr_map_t)
-HASH_MAP_SPEC_DELETE(static, isr_map_t)
-HASH_MAP_SPEC_PUT(static, isr_map_t, uint32_t, intc_internal_t*)
-HASH_MAP_SPEC_GET(static, isr_map_t, uint32_t, intc_internal_t*)
-HASH_MAP_SPEC_CONTAINS_KEY(static, isr_map_t, uint32_t)
-HASH_FUNCS_VALUE(isr_map_t, uint32_t)
-HASH_MAP_BODY_CREATE(static, isr_map_t)
-HASH_MAP_BODY_INITALISE(static, isr_map_t, MAX_ISRS, 16)
-HASH_MAP_BODY_DELETE(static, isr_map_t, MAX_ISRS, 16)
-HASH_MAP_BODY_PUT(static, isr_map_t, uint32_t, intc_internal_t*, MAX_ISRS, 16)
-HASH_MAP_BODY_GET(static, isr_map_t, uint32_t, intc_internal_t*, 16)
-HASH_MAP_BODY_CONTAINS_KEY(static, isr_map_t, uint32_t, 16)
-
 typedef struct intc_t
 {
 	mem_pool_info_t * pool;
-	isr_map_t * isr_map;
+	intc_internal_t * isrs[MAX_ISRS];
 	kernel_intc_t * kernel_intc;
 } intc_it_t;
 
@@ -53,20 +36,30 @@ intc_t * intc_create(mem_pool_info_t * const pool, kernel_intc_t * const kernel_
 	{
 		intc->kernel_intc = kernel_intc;
 		intc->pool = pool;
-		intc->isr_map = isr_map_t_create(
-			hash_basic_integer,
-			hash_equal_integer,
-			true,
-			pool);
+		for (int i = 0 ; i < MAX_ISRS ; i++)
+		{
+			intc->isrs[i] = NULL;
+		}
 	}
 	return intc;
 }
 
 void intc_delete(intc_t * const intc)
 {
-	if (intc && intc->isr_map)
+	if (intc)
 	{
-		isr_map_t_delete(intc->isr_map);
+		mem_pool_info_t * const pool = intc->pool;
+		if (pool != NULL)
+		{
+			for (int i = 0 ; i < MAX_ISRS ; i++)
+			{
+				if (intc->isrs[i] != NULL)
+				{
+					mem_free(pool, intc->isrs[i]);
+				}
+			}
+			mem_free(pool, intc);
+		}
 	}
 }
 
@@ -79,7 +72,11 @@ void intc_add_child(intc_t * const intc, const uint32_t cause, const intc_t * co
 		{
 			internal->type = CHILD_INTC;
 			internal->devices.child = child;
-			const bool_t put_ok = isr_map_t_put(intc->isr_map, cause, internal);
+			const bool_t put_ok = intc->isrs[cause] == NULL;
+			if (put_ok)
+			{
+				intc->isrs[cause] = internal;
+			}
 			kernel_assert("failed to add interrupt device to controller", put_ok);
 		}
 	}
@@ -94,7 +91,11 @@ void intc_add_device(intc_t * const intc, const uint32_t cause, const kernel_dev
 		{
 			internal->type = DEVICE_INTC;
 			internal->devices.device = device;
-			const bool_t put_ok = isr_map_t_put(intc->isr_map, cause, internal);
+			const bool_t put_ok = intc->isrs[cause] == NULL;
+			if (put_ok)
+			{
+				intc->isrs[cause] = internal;
+			}
 			kernel_assert("failed to add interrupt device to controller", put_ok);
 		}
 	}
@@ -109,7 +110,11 @@ void intc_add_pipe(intc_t * const intc, const uint32_t cause, const object_pipe_
 		{
 			internal->type = PIPE_INTC;
 			internal->devices.pipe = pipe;
-			const bool_t put_ok = isr_map_t_put(intc->isr_map, cause, internal);
+			const bool_t put_ok = intc->isrs[cause] == NULL;
+			if (put_ok)
+			{
+				intc->isrs[cause] = internal;
+			}
 			kernel_assert("failed to add interrupt device to controller", put_ok);
 		}
 	}
@@ -124,7 +129,11 @@ void intc_add_timer(intc_t * const intc, const uint32_t cause, const timer_t * c
 		{
 			internal->type = TIMER_INTC;
 			internal->devices.timer = timer;
-			const bool_t put_ok = isr_map_t_put(intc->isr_map, cause, internal);
+			const bool_t put_ok = intc->isrs[cause] == NULL;
+			if (put_ok)
+			{
+				intc->isrs[cause] = internal;
+			}
 			kernel_assert("failed to add interrupt device to controller", put_ok);
 		}
 	}
@@ -138,45 +147,38 @@ return_t intc_handle(const intc_t * const intc, tgt_context_t * const context)
 		uint32_t cause = 0;
 		if (intc->kernel_intc->get_cause(&cause, intc->kernel_intc->user_data))
 		{
-			if (isr_map_t_contains_key(intc->isr_map, cause))
+			intc_internal_t * const dev = intc->isrs[cause];
+			if (dev != NULL)
 			{
-				intc_internal_t * dev = NULL;
-				if (isr_map_t_get(intc->isr_map, cause, &dev))
+				switch(dev->type)
 				{
-					switch(dev->type)
+				case DEVICE_INTC:
+					ret = dev->devices.device->isr(dev->devices.device->user_data, cause);
+					break;
+				case CHILD_INTC:
+					ret = intc_handle(dev->devices.child, context);
+					break;
+				case PIPE_INTC:
+					ret = obj_pipe_send_message(
+							(object_pipe_t*)dev->devices.pipe,
+							0, /* no need, block is false */
+							PIPE_TX_SEND_AVAILABLE,
+							(void*)&cause, sizeof(cause),
+							false);
+					break;
+				case TIMER_INTC:
+					if (dev->devices.timer && dev->devices.timer->timer_isr)
 					{
-					case DEVICE_INTC:
-						ret = dev->devices.device->isr(dev->devices.device->user_data, cause);
-						break;
-					case CHILD_INTC:
-						ret = intc_handle(dev->devices.child, context);
-						break;
-					case PIPE_INTC:
-						ret = obj_pipe_send_message(
-								(object_pipe_t*)dev->devices.pipe,
-								0, /* no need, block is false */
-								PIPE_TX_SEND_AVAILABLE,
-								(void*)&cause, sizeof(cause),
-								false);
-						break;
-					case TIMER_INTC:
-						if (dev->devices.timer && dev->devices.timer->timer_isr)
-						{
-							ret = dev->devices.timer->timer_isr(context, dev->devices.timer->usr_data);
-						}
-						else
-						{
-							ret = UNKNOWN_INTERRUPT_CAUSE;
-						}
-						break;
-					case EMPTY_DEVICE:
-						ret = UNKNOWN_INTERRUPT_CAUSE;
-						break;
+						ret = dev->devices.timer->timer_isr(context, dev->devices.timer->usr_data);
 					}
-				}
-				else
-				{
+					else
+					{
+						ret = UNKNOWN_INTERRUPT_CAUSE;
+					}
+					break;
+				case EMPTY_DEVICE:
 					ret = UNKNOWN_INTERRUPT_CAUSE;
+					break;
 				}
 			}
 			else
