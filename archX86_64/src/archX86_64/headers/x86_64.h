f98ecfebd600683e5896b0c8090067f52788a633
Sam Truscott <sam@wumpus.co.uk>
2022-09-13 20:53:54 +0100

    Main build file for the x86 chain and initial newlib code
    The platform code still needs an impl

---
+++ archX86_64/src/archX86_64/headers/x86_64.h
@@ -0,0 +1,15 @@
+/*
+ *
+ * TINKER Source Code
+ *
+ *
+ *  [2009] - [2013] Samuel Steven Truscott
+ *  All Rights Reserved.
+ */
+#ifndef X86_64_H_
+#define X86_64_H_
+
+#include "ivt.h"
+#include "tgt.h"
+
+#endif /* X86_64_H_ */
